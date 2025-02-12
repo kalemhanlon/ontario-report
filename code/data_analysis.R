76e5b8a2f1b416d49b200158d5ffa95d98267deb
kalemhanlon <kh832@cornell.edu>
2025-02-12 15:40:28 -0500

    made tidyverse notes

--- code/data_analysis.R
+++ code/data_analysis.R
@@ -18,3 +18,25 @@ sample_data |>
   #calculate the mean
   summarise(ave_cells = mean(cells_per_ml))
 
+# filter: subset data by rows based on some value
+sample_data |> 
+  # subset samples only from the deep
+  filter(env_group == "Deep") |> 
+  #calculate the mean cell abundance
+  summarise(ave_cells = mean(cells_per_ml))
+
+# Mutate: create new data column 
+
+sample_data |> 
+  #calculate a new column with the total nitrogen to phosphorus ratio
+  mutate(tn_tp_ratio = total_nitrogen/total_phosphorus) |>
+  View()
+
+# Select: subset by entire columns
+sample_data |> 
+  #pick specific columns
+  select(sample_id:temperature)
+# select(-diss_org_carbon)
+
+
+#clean up data

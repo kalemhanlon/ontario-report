2d2693f955eb845fc785e4f8f2432b38edf49a76
kalemhanlon <kh832@cornell.edu>
2025-02-12 16:33:20 -0500

    tidied the data

--- code/data_analysis.R
+++ code/data_analysis.R
@@ -40,3 +40,80 @@ sample_data |>
 
 
 #clean up data
+
+taxon_dirty <- read_csv("data/taxon_abundance.csv", skip = 2)
+head(taxon_dirty)
+
+#only pick cyanobacteria 
+taxon_clean <- taxon_dirty |> 
+  select(sample_id:Cyanobacteria)
+dim(taxon_clean)#what are the wide format dimensions
+# pivot_longer: shape the data from wide into long format
+taxon_long <- taxon_clean |> 
+  #shape into log formatted data frame
+  pivot_longer(cols = Proteobacteria:Cyanobacteria, 
+               names_to = "Phylum", 
+               values_to = "Abundance")
+dim(taxon_long)
+
+#calculate average abundance of each phylum
+
+taxon_long |> 
+  group_by(Phylum) |> 
+  summarise(ave_abundance = mean(Abundance))
+
+#plot our data
+
+taxon_long |> 
+  ggplot(aes(x = sample_id, y = Abundance, fill = Phylum)) +
+  geom_col() + 
+  theme(axis.text.x = element_text(angle = 90))
+
+#joining data frames
+
+sample_data |> 
+  head(6)
+
+taxon_clean |> 
+  head(6)
+
+#inner_join
+sample_data |> 
+  inner_join(taxon_clean, by = "sample_id") |> 
+  dim()
+
+#intuition check on filtering joins
+length(unique(taxon_clean$sample_id))
+length(unique(sample_data$sample_id))
+
+#anti-join to see which rows are not joining
+sample_data |> 
+  anti_join(taxon_clean, by = "sample_id")
+
+#fixing september samples 
+taxon_clean_good_sept <- taxon_clean |> 
+  #replace sample id column with fixed september names
+  mutate(sample_id = str_replace(sample_id, "Sep", replacement = "September"))
+dim(taxon_clean_good_sept)
+
+#inner_join
+sample_and_taxon <- sample_data |> 
+  inner_join(taxon_clean_good_sept, by = "sample_id")
+ 
+#intuition check
+dim(sample_and_taxon)
+
+#test
+
+stopifnot(nrow(sample_and_taxon) == nrow(sample_data))
+
+# write out our clean data into a new file
+write_csv(sample_and_taxon, "data/sample_and_taxon.csv")
+
+#quick plot of chloroflexi
+
+sample_and_taxon |> 
+  ggplot(aes(x = depth, y = Chloroflexi)) +
+  geom_point() + 
+  #add a statistical model
+  geom_smooth()
